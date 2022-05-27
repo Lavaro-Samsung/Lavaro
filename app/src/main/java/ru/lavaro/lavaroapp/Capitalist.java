5e4f82060915e16627184794dba13ace3ff5f3d5
OlegMoiseev3229 <oleggg.moiseev@gmail.com>
2022-05-27 18:46:04 +0300

    initial commit

---
+++ app/src/main/java/ru/lavaro/lavaroapp/Capitalist.java
@@ -0,0 +1,10 @@
+package ru.lavaro.lavaroapp;
+
+public class Capitalist extends Person{
+
+    public Capitalist(String login, String password, String info) {
+        this.login = login;
+        this.password = password;
+        this.info = info;
+    }
+}
---
+++ app/src/main/java/ru/lavaro/lavaroapp/Worker.java
@@ -0,0 +1,14 @@
+package ru.lavaro.lavaroapp;
+
+import java.util.Objects;
+
+public class Worker extends Person {
+
+
+    public Worker(String login, String password, String info) {
+        this.login = login;
+        this.password = password;
+        this.info = info;
+    }
+
+}
